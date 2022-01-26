8635107a29b279df26c25848a20b32e951dc9359
Somtozech <ezechinnaemeka@gmail.com>
2022-01-26 23:03:43 +0100

    refactor: log exceptions in console

--- src/config/winston.config.js
+++ src/config/winston.config.js
@@ -1,46 +1,46 @@
 const winston = require('winston');
 
-const  options = {
-    error: {
-      level: 'error',
-      filename: `logs/error.log`,
-      json: true,
-      maxsize: 5242880, // 5MB
-      maxFiles: 5,
-    },
-  };
+const options = {
+  error: {
+    level: 'error',
+    filename: `logs/error.log`,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+  },
+};
 
 const logger = winston.createLogger({
-    transports: [
-      // - Write all logs with importance level of `error` or less to `error.log`
-      new winston.transports.File(options.error),
-    ],
-    exceptionHandlers: [
-        new winston.transports.File({ filename: 'logs/exceptions.log' })
-    ]
-  });
-  
-  //
-  // If we're not in production then log to the `console` with the format:
-  if (process.env.NODE_ENV !== 'production') {
-    const formatter = winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        winston.format.splat(),
-        winston.format.printf((info) => {
-          const { timestamp, level, message, ...meta } = info;
-        
-          return `${timestamp} [${level}]: ${message} ${
-            Object.keys(meta).length ? JSON.stringify(meta, null, 2) : ''
-          }`;
-        }),
-       );
+  transports: [
+    // - Write all logs with importance level of `error` or less to `error.log`
+    new winston.transports.File(options.error),
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'logs/exceptions.log' }),
+    new winston.transports.Console({ format: winston.format.simple(), })
+  ]
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+if (process.env.NODE_ENV !== 'production') {
+  const formatter = winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.splat(),
+    winston.format.printf((info) => {
+      const { timestamp, level, message, ...meta } = info;
+
+      return `${timestamp} [${level}]: ${message} ${Object.keys(meta).length ? JSON.stringify(meta, null, 2) : ''
+        }`;
+    }),
+  );
 
 
-    logger.add(new winston.transports.Console({
-      format:formatter,
-    }));
+  logger.add(new winston.transports.Console({
+    format: formatter,
+  }));
 
-  }
+}
 
 module.exports = logger;
