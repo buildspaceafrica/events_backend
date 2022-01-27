91a191690d3d10a35fecd11b72aeda61610f91dc
Somtozech <ezechinnaemeka@gmail.com>
2022-01-27 16:53:21 +0100

    fix: Removed agenda defaultConcurrency

--- src/config/agenda.config.js
+++ src/config/agenda.config.js
@@ -2,11 +2,9 @@ const Agenda = require("agenda");
 const { DB_URI } = require("./index");
 
 const connectionOpts = {
-	db: { address: DB_URI, collection: "jobs" },
-	defaultConcurrency: 1,
+  db: { address: DB_URI, collection: "jobs" },
 };
 
 const agenda = new Agenda(connectionOpts);
 
 module.exports = agenda;
-
